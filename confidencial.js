0f3bd359a28c0a5e7e625fca2a9724d5110b46ad
Vinicius Kato <vinynichan@gmail.com>
2024-05-16 20:10:12 -0300

    Commit with sign in function

--- confidencial.js
+++ confidencial.js
@@ -1,24 +1,2 @@
-const users = [
-    { username: 'vinynichan', password: 'linkinviny'},
-    { username: 'victykato', password: 'bitikun1'},
-]
-
-function login(username, password){
-    const user = users.find(user => user.username === username);
-    if(!user){
-        return false;
-    }
-    if(user.password === password){
-        return true;
-    }
-  return false;  
-}
-
-const username = 'vinynichan';
-const password = 'linkinviny';
-
-if(login(username, password)){
-    console.log('Login Successfull!');    
-}else{
-    console.log('Invalid Username or Password.');
-}
+console.log('login: vinikato');
+console.log('password: chapokafifa');
